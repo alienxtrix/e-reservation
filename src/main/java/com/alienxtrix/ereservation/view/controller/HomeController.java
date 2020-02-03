8991ef16bc086118e14c5ad5e8e7b24b445bec7f
Josh Wolf <alienxtrix@gmail.com>
2020-02-03 04:49:08 -0600

    login personalizado completo responsivo y con spring security implement

---
+++ src/main/java/com/alienxtrix/ereservation/view/controller/HomeController.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.alienxtrix.ereservation.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author josh Fuentes
+ *
+ */
+@Controller
+@RequestMapping("app/home")
+public class HomeController {
+	
+	
+
+}
